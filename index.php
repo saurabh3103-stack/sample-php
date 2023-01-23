539eb8a1b6a56c2f68838a600c97365f091e84ce
saurabh3103-stack <75486759+saurabh3103-stack@users.noreply.github.com>
2023-01-23 20:54:26 +0530

    update index page

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ document.onkeydown = function(e) {
 <script type="text/javascript">
 document.onmousedown=disableclick;
 status="Right Click Disabled";
-Function disableclick(event)
+function disableclick(event)
 {
   if(event.button==2)
    {
