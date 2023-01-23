181c5aa916a9790d45572a3284bc8a1284bb238a
saurabh3103-stack <75486759+saurabh3103-stack@users.noreply.github.com>
2023-01-23 22:55:21 +0530

    update index page

--- index.php
+++ index.php
@@ -116,6 +116,38 @@ function disableclick(event)
    }
 }
 </script>
+<script type="text/javascript">
+    function Captcha(){
+     var alpha = new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 
+            '0','1','2','3','4','5','6','7','8','9');
+     var i;
+     for (i=0;i<6;i++){
+         var a = alpha[Math.floor(Math.random() * alpha.length)];
+         var b = alpha[Math.floor(Math.random() * alpha.length)];
+         var c = alpha[Math.floor(Math.random() * alpha.length)];
+         var d = alpha[Math.floor(Math.random() * alpha.length)];
+         var e = alpha[Math.floor(Math.random() * alpha.length)];
+         var f = alpha[Math.floor(Math.random() * alpha.length)];
+         var g = alpha[Math.floor(Math.random() * alpha.length)];
+                      }
+         var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' '+ f + ' ' + g;
+         document.getElementById("mainCaptcha").innerHTML = code
+         document.getElementById("mainCaptcha").value = code
+       }
+function ValidCaptcha(){
+     var string1 = removeSpaces(document.getElementById('mainCaptcha').value);
+     var string2 =         removeSpaces(document.getElementById('txtInput').value);
+     if (string1 == string2){
+            return true;
+     }else{        
+          return false;
+          }
+}
+function removeSpaces(string){
+     return string.split(' ').join('');
+}
+</script>
 
 	<!--[if lt IE 7]>
     <div style=' clear: both; text-align:center; position: relative;'>
@@ -333,7 +365,7 @@ function newPopup(url) {
 																						<span class=""><input type="text" style="width:200px; font-family: Verdana; font-size: 10pt; padding:4px;" name="pid"></span>
 																					</label>
 																				</fieldset>
-																				   <img src="captcha.php" alt="CAPTCHA" class="captcha-image"><p style="
+																				 <h2 type="text" id="mainCaptcha"></h2><p style="
     color: antiquewhite; margin: 0px !important;
 ">
         Can't read the image? click <a href='javascript: refreshCaptcha();' style="
