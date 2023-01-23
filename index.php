8baeadd0fa6a4cf4c938aadbf2655ac3ce3eedd4
saurabh3103-stack <75486759+saurabh3103-stack@users.noreply.github.com>
2023-01-24 00:51:02 +0530

    update index page

--- index.php
+++ index.php
@@ -386,10 +386,12 @@ function newPopup(url) {
 																					</label>
 																				</fieldset>
                                                                             
-                                                                                <h2 type="text" id="mainCaptcha"></h2>    
+                                   <div class="row">
+                                                         <h2 type="text" id="mainCaptcha" style="float: left"></h2>    
                                                                              
-	<p><input type="button" id="refresh" onclick="Captcha();"/></p> 
-									<div class="">
+    <p><input type="button" id="refresh" onclick="Captcha();"/></p>
+                                   </div>                            
+									<div class="row">
 										
         <strong class="d-block color-1 margin-bot2">Please Enter code :</strong>
         <input id="captcha_code" name="captcha_code" type="text"></td>
