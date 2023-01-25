aef2fb73d91035ccbc3b75fcc2b671dbd5483a95
saurabh3103-stack <75486759+saurabh3103-stack@users.noreply.github.com>
2023-01-25 11:29:24 +0530

    update detailsby page

--- detailsByPropnonew.php
+++ detailsByPropnonew.php
@@ -151,8 +151,106 @@ WindowObject.close();
 
 </script>
 
+<style>
+.grid_12{margin-left:0px;}
+    @media only print {
+         .bodyy-content {
+            visibility: hidden;
+         }
+         .cssInp {
+            visibility: visible;
+         }
+      }
+h3.text-color.bill-heading{margin: 19px;}
+
+@media only screen and (max-width: 767px) {
+h3.text-color.bill-heading{margin: 0 !important;}
+}
+/* Popup box BEGIN */
+.hover_bkgr_fricc{
+    background:rgba(0,0,0,.4); 
+    display:none;
+    height:100%;
+    position:fixed;
+    text-align:center;
+    top:0;
+    width:100%;
+    z-index:10000;
+}
+.pay-now{background: green;
+    color: #fff;
+    border-radius: 5px;
+    padding: 7px;
+    border: none;
+    font-size:17px;
+    cursor: pointer;
+    width: max-content;}
+.hover_bkgr_fricc .helper{
+    display:inline-block;
+    height:100%;
+    vertical-align:middle;
+}
+.hover_bkgr_fricc > div {
+    background-color: #fff;
+    box-shadow: 10px 10px 60px #555;
+    display: inline-block;
+    height: auto;
+    max-width: 551px;
+    min-height: 100px;
+    vertical-align: middle;
+    width: 60%;
+    position: relative;
+    border-radius: 8px;
+    padding: 15px 5%;
+}
+.popupCloseButton {
+    background-color: #fff;
+    border: 3px solid #999;
+    border-radius: 50px;
+    cursor: pointer;
+    display: inline-block;
+    font-family: arial;
+    font-weight: bold;
+    position: absolute;
+    top: -20px;
+    right: -20px;
+    font-size: 25px;
+    line-height: 30px;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+}
+.popupCloseButton:hover {
+    background-color: #ccc;
+}
+.trigger_popup_fricc {
+    cursor: pointer;
+    font-size: 20px;
+    margin: 20px;
+    display: inline-block;
+    font-weight: bold;
+}
+/* Popup box BEGIN */
+.border{
+ border: 2px solid #a10000 !important;   
+}
+.text-color{
+    color: #a10000;
+}
+.table tr td {
+  border: 1px solid #a10000;
+padding: 0px 0px 0px 9px;
+}
+.payment_history{
+    font-size: 15px;
+    font-weight: 600;
+}
+.payment_historyy {padding: 5px !important;}
+.text-right a:hover{color:#fff;}
+.payment_historyyy td{padding:.75rem !important;}
+   </style>
 </head>
-<body id="page1" class="bodyy-content" oncontextmenu="return false" style="background: #fff8c6;">            
+<body id="page1" class="bodyy-content" oncontextmenu="return false" style="background: #fff8c6;">    
 <!--=========================================header=============================================-->
                 <div class="hover_bkgr_fricc">
     <span class="helper"></span>
