2d621c88f53fede1f7ae54fe11c790324cc51929
saurabh3103-stack <75486759+saurabh3103-stack@users.noreply.github.com>
2023-01-28 16:51:40 +0530

    update test page

--- test.php
+++ test.php
@@ -1,25 +1,26 @@
 <?php
-$client = new http\Client;
-$request = new http\Client\Request;
-$request->setRequestUrl('https://knndigitalpayment.com/AppSearchpid.php');
-$request->setRequestMethod('POST');
-$body = new http\Message\Body;
-$body->addForm(array(
-  'hno' => '1',
-  'zone' => '6',
-  'ward' => '16',
-  'name' => 'Vi',
-  'chk' => 'Select'
-), array(
-
+echo "6016MDP03903001";
+$curl = curl_init();
+curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://knndigitalpayment.com/AppBillinfo.php',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_SSL_VERIFYPEER=> FALSE,
+            CURLOPT_SSL_VERIFYHOST=>FALSE,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('GisId' => "6016MDP03903001"),
+            CURLOPT_HTTPHEADER => array(
+                        'X-TP-ApiKey: c77f5eb4d7d525855522d7ac65c5487d'
+            ),
 ));
-$request->setBody($body);
-$request->setOptions(array());
-$request->setHeaders(array(
-  'X-TP-ApiKey' => 'c77f5eb4d7d525855522d7ac65c5487d'
-));
-$client->enqueue($request)->send();
-$response = $client->getResponse();
-echo $response->getBody();
 
+$response = curl_exec($curl);
+$json = json_decode($response, true);
+$err=curl_error($curl);
+print_r($response);
+print_r($err);
 ?>
